777f729ade46ca54acd5c5fc79a6f66f823b2035
Yauhen <silent_control@tut.by>
2018-10-29 19:51:34 +0300

    Podcast 01: Lazy (end code)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, Component, Suspense } from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 import Home from './components/home';
-import Profile from './components/profile';
 import Contacts from './components/contacts';
 
+const Profile = React.lazy(() => import('./components/profile'));
+
 class App extends Component {
+  state = {
+    showProfile: false
+  };
+
+  showProfileToggle = () => {
+    this.setState(({ showProfile }) => ({
+      showProfile: !showProfile,
+    }));
+  };
+
   render() {
+    const { showProfile } = this.state;
+
     return (
-      <BrowserRouter>
-        <Fragment>
-          <nav>
-            <NavLink style={{marginRight: '20px'}} to="/profile">Profile Page</NavLink>
-            <NavLink to="/contacts">Contacts Page</NavLink>
-          </nav>
-          <Route path="/" exact component={Home} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/contacts" component={Contacts} />
-        </Fragment>
-      </BrowserRouter>
+      <Fragment>
+        <button onClick={this.showProfileToggle}>Toggle Profile</button>
+        {showProfile ? (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Profile />
+          </Suspense>
+        ) : (
+          <Contacts />
+        )}
+      </Fragment>
+      // <BrowserRouter>
+      //   <Fragment>
+      //     <nav>
+      //       <NavLink style={{marginRight: '20px'}} to="/profile">Profile Page</NavLink>
+      //       <NavLink to="/contacts">Contacts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" exact component={Home} />
+      //     <Route
+      //       path="/profile"
+      //       render={() => (
+      //         <Suspense fallback={<div>Loading...</div>}>
+      //           <Profile />
+      //         </Suspense>
+      //       )}
+      //     />
+      //     <Route path="/contacts" component={Contacts} />
+      //   </Fragment>
+      // </BrowserRouter>
     );
   }
 }
