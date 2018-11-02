4513fc6a65fc40f7eeb233dd4d0688c6d3dce4a1
Yauhen <silent_control@tut.by>
2018-11-02 12:02:36 +0300

    Podcast 02: useState (end code)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Item from './Item';
 import { ThemeContext, PodcastContext } from './app-context';
 
 const App = () => {
   const podcast = useContext(PodcastContext);
   const theme = useContext(ThemeContext);
+  const [count, setCount] = useState(0);
+  const [value, changeValue] = useState('Test value');
+
+  const handleValueChange = (e) => {
+    changeValue(e.target.value);
+  }
 
   return (
     <div className={`card ${theme}`}>
       <Item label="language">
         <h4>{podcast}</h4>
+      </Item>
+
+      <Item label={`Counter value is ${count}`}>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+      </Item>
+
+      <Item label={`${value}`}>
+        <input
+          value={value}
+          onChange={handleValueChange}
+        />
       </Item>
     </div>
   );
