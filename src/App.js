9bb2b368c1a04fd769c45ec8ad32ca6e59c73be6
Yauhen <silent_control@tut.by>
2018-11-02 14:22:53 +0300

    Podcast 02: useEffect (end code)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Item from './Item';
 import { ThemeContext, PodcastContext } from './app-context';
 
@@ -7,10 +7,31 @@ const App = () => {
   const theme = useContext(ThemeContext);
   const [count, setCount] = useState(0);
   const [value, changeValue] = useState('Test value');
+  const [width, setWidth] = useState(window.innerWidth);
+  const [user, setUser] = useState(null);
 
   const handleValueChange = (e) => {
     changeValue(e.target.value);
   }
+
+  useEffect(() => {
+    document.title = value;
+  });
+
+  useEffect(() => {
+    const handleWidth = setWidth(window.innerWidth)
+    window.addEventListener('resize', handleWidth);
+    return () => {
+      window.removeEventListener('resize', handleWidth);
+    };
+  });
+
+  useEffect(async () => {
+    const response = await fetch('https://api.randomuser.me/');
+    const data = await response.json();
+    const [item] = data.results;
+    setUser(item);
+  }, [count]);
 
   return (
     <div className={`card ${theme}`}>
@@ -28,6 +49,15 @@ const App = () => {
           onChange={handleValueChange}
         />
       </Item>
+
+      <Item label={`Browser window is`}>
+        <h4>{width}</h4>
+      </Item>
+
+      <Item label="Fetch User">
+        <div>{user ? `${user.name.first} ${user.name.last}` : ''}</div>
+      </Item>
+
     </div>
   );
 };
