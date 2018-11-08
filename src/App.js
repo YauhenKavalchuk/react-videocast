3702a6c6962a2a7db3bc4d28fb9c232ebcadfb7f
Yauhen <silent_control@tut.by>
2018-11-08 18:19:29 +0300

    Minor functionality fix

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   });
 
   useEffect(() => {
-    const handleWidth = setWidth(window.innerWidth)
+    const handleWidth = () => setWidth(window.innerWidth)
     window.addEventListener('resize', handleWidth);
     return () => {
       window.removeEventListener('resize', handleWidth);
